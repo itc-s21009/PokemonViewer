2338b688e867d9ee22ad5d34daae001174247ff6
itc-s21009 <s21009@std.it-college.ac.jp>
2022-08-18 16:11:17 +0900

    viewBinding

--- src/main/java/jp/ac/it_college/std/s21009/pokemonviewer/MainActivity.kt
+++ src/main/java/jp/ac/it_college/std/s21009/pokemonviewer/MainActivity.kt
@@ -2,10 +2,21 @@ package jp.ac.it_college.std.s21009.pokemonviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s21009.pokemonviewer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val pokemonList = mapOf<String, Int>(
+        "フシギダネ" to 1,
+        "ヒトカゲ" to 4,
+        "ゼニガメ" to 7,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }

857b37c8f0cbeaefc785ab3269d730ac82020f3e
itc-s21009 <s21009@std.it-college.ac.jp>
2022-08-18 16:58:12 +0900

    ポケモン表示できた

--- src/main/java/jp/ac/it_college/std/s21009/pokemonviewer/MainActivity.kt
+++ src/main/java/jp/ac/it_college/std/s21009/pokemonviewer/MainActivity.kt
@@ -2,12 +2,33 @@ package jp.ac.it_college.std.s21009.pokemonviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toolbar
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.lifecycleScope
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.picasso.Picasso
 import jp.ac.it_college.std.s21009.pokemonviewer.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s21009.pokemonviewer.service.PokemonInfo
+import jp.ac.it_college.std.s21009.pokemonviewer.service.PokemonService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+
+private const val BASE_URL = "https://pokeapi.co/"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val pokemonList = mapOf<String, Int>(
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val pokemonList = mapOf(
         "フシギダネ" to 1,
         "ヒトカゲ" to 4,
         "ゼニガメ" to 7,
@@ -18,5 +39,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.spPokemon.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            pokemonList.keys.toTypedArray()
+        )
+        binding.btDisplay.setOnClickListener {
+            val id = pokemonList[binding.spPokemon.selectedItem]
+            showPokemonInfo(id?:throw IllegalArgumentException("存在しないポケモンが選ばれました"))
+        }
     }
+
+    @UiThread
+    private fun showPokemonInfo(id: Int) {
+        lifecycleScope.launch {
+            val info = getPokemonInfo(id)
+            Picasso.get().load(info.sprites.other.officialArtwork.frontDefault).into(binding.imgPokemon)
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    @WorkerThread
+    private suspend fun getPokemonInfo(id: Int): PokemonInfo {
+        return withContext(Dispatchers.IO) {
+            val retrofit = Retrofit.Builder().apply {
+                baseUrl(BASE_URL)
+                addConverterFactory(MoshiConverterFactory.create(moshi))
+            }.build()
+            val service: PokemonService = retrofit.create(PokemonService::class.java)
+            service.fetchPokemon(id).execute().body() ?: throw IllegalStateException("ポケモンが取れませんでした")
+        }
+    }
+
 }
--- src/main/java/jp/ac/it_college/std/s21009/pokemonviewer/service/PokemonService.kt
+++ src/main/java/jp/ac/it_college/std/s21009/pokemonviewer/service/PokemonService.kt
@@ -2,9 +2,11 @@ package jp.ac.it_college.std.s21009.pokemonviewer.service
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-abstract class PokemonService {
-    @GET("api/v2/pokemon/")
-    abstract fun fetchWeather(
+interface PokemonService {
+    @GET("api/v2/pokemon/{id}")
+    fun fetchPokemon(
+        @Path("id") id: Int
     ): Call<PokemonInfo>
 }
